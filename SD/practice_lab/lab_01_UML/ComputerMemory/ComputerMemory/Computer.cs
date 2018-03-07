728b0c94b72651b340e724c4c26710b4fa96ac06
Ács Dávid <acsdavid97@gmail.com>
2018-03-07 21:04:07 +0200

    first SD laboratory exercises

---
+++ SD/practice_lab/lab_01_UML/ComputerMemory/ComputerMemory/Computer.cs
@@ -0,0 +1,19 @@
+﻿namespace ComputerMemory
+{
+    public class Computer
+    {
+        private Memory _theMemory;
+
+        public Computer(Computer another)
+        {
+            if (another._theMemory is Memory1)
+            {
+                _theMemory = new Memory1(another._theMemory);
+            }
+            else
+            {
+                _theMemory = new Memory(another._theMemory);
+            }
+        }
+    }
+}
---
+++ SD/practice_lab/lab_01_UML/ComputerMemory/ComputerMemory/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ComputerMemory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
